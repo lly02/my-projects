d641e7ecb320e2cee6245ea85b7bc3b04b576712
lly02 <lly.02@outlook.com>
2022-08-07 16:25:09 +0800

    generate and populate invoice

--- invoice-generator/main.py
+++ invoice-generator/main.py
@@ -1,18 +1,50 @@
 import os
-import docx
 
+from docx import Document
 from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
 
 
-if __name__ == "__main__" :
-    # Get current working dir path
+def generate_invoice(invoice_data, path, file_number):
+    document = Document(template)
+    table = document.tables[0]
+
+    # fill up invoice
+    table.rows[3].cells[2].text = str(invoice_data[7]) + "(" + str(invoice_data[0]) + ")"  # phone + customer
+    table.rows[5].cells[2].text = str(invoice_data[2]) + str(invoice_data[6])   # street adr + postcode
+    table.rows[6].cells[2].text = str(invoice_data[8])  # email
+
+    # invoice name
+    document.save(path + "Invoice_" + str(file_number) + ".docx")
+
+
+if __name__ == "__main__":
+    # paths
     cwd = os.getcwd()
-    data = cwd + "\\static\\data\\details.xlsx"
+    static_path = cwd + "\\static\\data\\"
+    data = static_path + "details.xlsx"
+    template = static_path + "template.docx"
+    generate = static_path + "\\generated_files\\"
 
+    # init workbook
     wb = load_workbook(data)
     ws = wb.active
 
-    data_range = ws[]
+    # loop thru excel data
+    for count, row in enumerate(ws.values):
+        row_data = []
+        print(row)
 
-    print(wb.active)
+        for cell in row:
+            # skip header
+            if count == 0:
+                continue
+
+            else:
+                if cell is None:
+                    row_data.append("")
+                else:
+                    row_data.append(cell)
+
+        # if not header, generate invoice
+        if count != 0:
+            generate_invoice(row_data, generate, count)
