70b1caec0c5d8ed77f5c239f827325510314a6ad
lly02 <lly.02@outlook.com>
2022-08-22 22:38:19 +0800

    calorie-tracker scrapper GUI done

---
+++ applications/calorie-tracker/scrapper/GUI.py
@@ -0,0 +1,127 @@
+import tkinter as tk
+import re
+import math
+import database.dummy as db
+
+
+class Counter(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        # main frame
+        self.title("Calorie Tracker")
+        self.geometry("800x600")
+        self.resizable(0, 0)
+
+        # string var
+        self.query = tk.StringVar()
+
+        # header frame
+        self.frm_header = tk.Frame(self, relief=tk.RAISED, borderwidth=5, bg="gray")
+        self.frm_header.grid_columnconfigure(0, weight=1)
+        self.frm_header.pack(fill=tk.X)
+
+        # search bar
+        self.ent_search = tk.Entry(self.frm_header, textvariable=self.query, font=25)
+        self.ent_search.bind("<Return>", self.search)
+        self.ent_search.grid(column=0, row=0, pady=10, padx=10, sticky="nsew")
+
+        # enter button
+        self.btn_enter = tk.Button(self.frm_header, text="Search", height=1, font=25, command=self.search)
+        self.btn_enter.grid(column=1, row=0, pady=10, padx=10)
+
+        # result frame
+        self.frm_result = tk.Frame(self, relief=tk.RAISED, bg="gray")
+        self.frm_result.pack(fill=tk.BOTH, expand=True)
+        self.frm_result.columnconfigure(0, weight=1)
+
+        # pagination bar
+        self.frm_pagination = tk.Frame(self, bg="gray")
+        self.frm_pagination.pack(fill=tk.BOTH)
+
+        # pagination buttons wrap
+        self.frm_pagination_button = tk.Frame(self.frm_pagination, bg="gray")
+        self.frm_pagination_button.pack(ipady=15)
+
+    def search(self, *args):
+        pattern = f"^.*{self.query.get()}.*$"
+        results = [x for x in db.data if re.fullmatch(pattern, x[0], re.IGNORECASE)]
+
+        if len(results) == 0:
+            print("Nothing found!")
+        else:
+            self.display_results(results)
+
+        self.query.set("")
+
+    def display_results(self, results, page=1):
+        # clear the results
+        for widget in self.frm_result.winfo_children():
+            widget.destroy()
+        for widget in self.frm_pagination_button.winfo_children():
+            widget.destroy()
+
+        results_buttons = [
+            tk.Button(
+                self.frm_result,
+                text=result[0] + "\n" + result[1],
+                justify=tk.LEFT,
+                anchor="nw",
+                borderwidth=5,
+                font=25
+            )
+            for result in results
+        ]
+
+        # insert result buttons into the result frame
+        # try except for when len of last page is lesser than 5
+        try:
+            for count, x in enumerate(range((page - 1) * 5, page * 5)):
+                results_buttons[x].grid(
+                    row=count,
+                    column=0,
+                    sticky="ew",
+                    padx=10,
+                    pady=10
+                )
+        except IndexError:
+            pass
+
+        # pagination buttons (<, ..., >)
+        btn_pagination = [
+            tk.Button(
+                self.frm_pagination_button,
+                text="<",
+                command=lambda: self.display_results(results, 1 if page - 1 == 0 else page - 1)
+            ),
+            tk.Button(
+                self.frm_pagination_button, text=">",
+                command=lambda: self.display_results(results,
+                                                     page if page * 5 >= len(results_buttons) else page + 1)
+            )
+        ]
+        # actual pagination numbers
+        btn_pagination[1:1] = [
+            tk.Button(
+                self.frm_pagination_button,
+                text=x,
+                command=lambda k=x: self.display_results(results, k)
+            )
+            if x != page
+            else tk.Button(
+                self.frm_pagination_button,
+                text=x,
+                command=lambda k=x: self.display_results(results, k),
+                bg="gray"
+            )
+            for x in range(1, math.ceil(len(results_buttons) / 5) + 1)
+        ]
+
+        for button in btn_pagination:
+            button.pack(side=tk.LEFT)
+
+
+if __name__ == "__main__":
+    counter = Counter()
+    counter.mainloop()
+
---
+++ applications/calorie-tracker/scrapper/database/dummy.py
@@ -0,0 +1,16 @@
+data = [
+        ("Chicken Breast", "230kcal"),
+        ("Broccoli", "26kcal"),
+        ("Egg", "60kcal"),
+        ("Noodle", "50kcal"),
+        ("Rice", "55kcal"),
+        ("Prawn", "10kcal"),
+        ("Spinach", "25kcal"),
+        ("Oil", "100kcal"),
+        ("Crab", "120kcal"),
+        ("Cream", "200kcal"),
+        ("Cactus", "29kcal"),
+        ("Cake", "299kcal"),
+        ("Water", "0kcal"),
+        ("Mushroom", "36kcal")
+        ]
